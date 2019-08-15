08326bb682e65360ba3761e1a8c2aae68c7bc8fd
renan.sugiyama <renan.sugiyama@gmail.com>
2019-08-15 20:22:46 -0300

    send data from a component to another and create directive to manipulate the dropdown buttons

--- recipes-project/src/app/app.component.ts
+++ recipes-project/src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'recipes-project';
+  loadedFeature = 'recipe';
+
+  onNavegate(feature: string) {
+    this.loadedFeature = feature;
+  }
+
 }
--- recipes-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipes-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,14 +7,22 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  @Output() selectedRecipe = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
     new Recipe('pie', 'fast and delicious', 'https://cdn.pixabay.com/photo/2017/05/17/02/51/apple-pie-2319691_960_720.jpg'),
-    new Recipe('pie', 'fast and delicious', 'https://cdn.pixabay.com/photo/2017/05/17/02/51/apple-pie-2319691_960_720.jpg'),
+    new Recipe('ice-cream', 'super fat', 'https://cdn.pixabay.com/photo/2017/05/17/02/51/apple-pie-2319691_960_720.jpg'),
     new Recipe('pie', 'fast and delicious', 'https://cdn.pixabay.com/photo/2017/05/17/02/51/apple-pie-2319691_960_720.jpg')
   ];
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  selectRecipe(recipeItem: Recipe) {
+    this.selectedRecipe.emit(recipeItem);
   }
 
 }
