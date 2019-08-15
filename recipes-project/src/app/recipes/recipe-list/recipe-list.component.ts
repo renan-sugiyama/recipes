ce7eedda750b1b74d5c0833942886c5b11a380ee
renan.sugiyama <renan.sugiyama@gmail.com>
2019-08-15 11:50:25 -0300

    added some inicial code in compononents

--- recipes-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipes-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe('pie', 'fast and delicious', 'https://cdn.pixabay.com/photo/2017/05/17/02/51/apple-pie-2319691_960_720.jpg'),
+    new Recipe('pie', 'fast and delicious', 'https://cdn.pixabay.com/photo/2017/05/17/02/51/apple-pie-2319691_960_720.jpg'),
+    new Recipe('pie', 'fast and delicious', 'https://cdn.pixabay.com/photo/2017/05/17/02/51/apple-pie-2319691_960_720.jpg')
+  ];
   constructor() { }
 
   ngOnInit() {
