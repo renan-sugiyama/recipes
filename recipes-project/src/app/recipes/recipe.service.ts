02210ccea1e5a7a51a3c8d2899859e4c8a435cc5
renan.sugiyama <renan.sugiyama@gmail.com>
2019-08-17 23:06:36 -0300

    insert firebase to storage and fetch data

--- recipes-project/src/app/recipes/recipe.service.ts
+++ recipes-project/src/app/recipes/recipe.service.ts
@@ -8,31 +8,36 @@ import { Injectable } from '@angular/core';
 })
 export class RecipeService {
   recipeChanged = new Subject<Recipe[]>();
+  private recipes: Recipe[] = [];
+  // private recipes: Recipe[] = [
+  //   new Recipe('pie',
+  //     'fast and delicious',
+  //     'https://cdn.pixabay.com/photo/2017/05/17/02/51/apple-pie-2319691_960_720.jpg',
+  //     [
+  //       new Ingredient('chicken', 1),
+  //       new Ingredient('tomatos', 3),
+  //     ]),
+  //   new Recipe(
+  //     'ice-cream',
+  //     'super fat',
+  //     'https://upload.wikimedia.org/wikipedia/commons/d/da/Strawberry_ice_cream_cone_%285076899310%29.jpg',
+  //     [
+  //       new Ingredient('chocolate', 1),
+  //       new Ingredient('milk', 3),
+  //     ]),
+  //   new Recipe('pie',
+  //     'fast and delicious',
+  //     'https://cdn.pixabay.com/photo/2017/05/17/02/51/apple-pie-2319691_960_720.jpg',
+  //     [
+  //       new Ingredient('chicken', 1),
+  //       new Ingredient('tomatos', 3),
+  //     ])
+  // ];
 
-  private recipes: Recipe[] = [
-    new Recipe('pie',
-      'fast and delicious',
-      'https://cdn.pixabay.com/photo/2017/05/17/02/51/apple-pie-2319691_960_720.jpg',
-      [
-        new Ingredient('chicken', 1),
-        new Ingredient('tomatos', 3),
-      ]),
-    new Recipe(
-      'ice-cream',
-      'super fat',
-      'https://upload.wikimedia.org/wikipedia/commons/d/da/Strawberry_ice_cream_cone_%285076899310%29.jpg',
-      [
-        new Ingredient('chocolate', 1),
-        new Ingredient('milk', 3),
-      ]),
-    new Recipe('pie',
-      'fast and delicious',
-      'https://cdn.pixabay.com/photo/2017/05/17/02/51/apple-pie-2319691_960_720.jpg',
-      [
-        new Ingredient('chicken', 1),
-        new Ingredient('tomatos', 3),
-      ])
-  ];
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
+    this.recipeChanged.next(this.recipes.slice());
+  }
 
   getRecipes() {
     return this.recipes.slice();
